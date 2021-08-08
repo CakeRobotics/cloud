b3e86367e0e61228a51e44453002aef95fc9aa6f
Mostafa Farzan <farzan@cakerobotics.com>
2021-08-08 14:17:05 +0430

    Reword

--- main-website/src/App.js
+++ main-website/src/App.js
@@ -39,7 +39,7 @@ class App extends React.Component {
             <CarouselItem interval="10000">
               <CarouselImage imgkey="code-efficiently"/>
               <Carousel.Caption>
-                <h1>Programming Robots Efficiently</h1>
+                <h1>Efficient Robot Programming</h1>
                 <h4>Our high-level libraries allow you to focus on the task.</h4>
               </Carousel.Caption>
             </CarouselItem>
