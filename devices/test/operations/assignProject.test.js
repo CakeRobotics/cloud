88739b392d96352e1e8e87205b8e500f9876309e
Mostafa Farzan <farzan@cakerobotics.com>
2022-02-26 16:04:34 +0330

    Add assignProject tests

--- devices/test/operations/assignProject.test.js
+++ devices/test/operations/assignProject.test.js
@@ -3,7 +3,7 @@ const getHttpAgent = require('../utils/getHttpAgent');
 const randomString = require('../utils/randomString');
 const { testUsers } = require('../utils/testUsers');
 
-test('Create Device (OK)', async function() {
+test('Assign Project (OK)', async function() {
     const deviceName = `device-${randomString()}`;
     const projectId = `project-${randomString()}`;
     const projectFullId = `${testUsers.alice.username}/${projectId}`;
@@ -35,4 +35,73 @@ test('Create Device (OK)', async function() {
     expect(response.statusCode).toEqual(StatusCodes.OK);
     const { project } = response.body.find(device => device.name === deviceName);
     expect(project).toEqual(projectFullId);
+});
+
+test('Assign Project (Unset Others)', async function() {
+    const deviceNames = [`device-${randomString()}`, `device-${randomString()}`, `device-${randomString()}`];
+    const projectId = `project-${randomString()}`;
+    const projectFullId = `${testUsers.alice.username}/${projectId}`;
+
+    for (const deviceName of deviceNames) {
+        var response = await
+            getHttpAgent()
+            .post('/')
+            .set('Authorization', `Bearer ${testUsers.alice.token}`)
+            .send({ name: deviceName });
+        expect(response.statusCode).toEqual(StatusCodes.CREATED);
+    }
+
+    var response = await
+        getHttpAgent()
+        .post(`/assign_project`)
+        .set('Authorization', `Bearer ${testUsers.alice.token}`)
+        .send({
+            devices: [
+                {owner: testUsers.alice.username, name: deviceNames[0]},
+                {owner: testUsers.alice.username, name: deviceNames[1]},
+                {owner: testUsers.alice.username, name: deviceNames[2]},
+            ],
+            project: projectFullId
+        });
+    expect(response.statusCode).toEqual(StatusCodes.OK);
+
+    var response = await
+        getHttpAgent()
+        .get('/')
+        .set('Authorization', `Bearer ${testUsers.alice.token}`)
+        .send();
+    expect(response.statusCode).toEqual(StatusCodes.OK);
+    var { project } = response.body.find(device => device.name === deviceNames[0]);
+    expect(project).toEqual(projectFullId);
+    var { project } = response.body.find(device => device.name === deviceNames[1]);
+    expect(project).toEqual(projectFullId);
+    var { project } = response.body.find(device => device.name === deviceNames[2]);
+    expect(project).toEqual(projectFullId);
+
+    var response = await
+        getHttpAgent()
+        .post(`/assign_project`)
+        .set('Authorization', `Bearer ${testUsers.alice.token}`)
+        .send({
+            devices: [
+                {owner: testUsers.alice.username, name: deviceNames[0]},
+                {owner: testUsers.alice.username, name: deviceNames[1]},
+            ],
+            project: projectFullId,
+            unsetOthers: true,
+        });
+    expect(response.statusCode).toEqual(StatusCodes.OK);
+
+    var response = await
+        getHttpAgent()
+        .get('/')
+        .set('Authorization', `Bearer ${testUsers.alice.token}`)
+        .send();
+    expect(response.statusCode).toEqual(StatusCodes.OK);
+    var { project } = response.body.find(device => device.name === deviceNames[0]);
+    expect(project).toEqual(projectFullId);
+    var { project } = response.body.find(device => device.name === deviceNames[1]);
+    expect(project).toEqual(projectFullId);
+    var { project } = response.body.find(device => device.name === deviceNames[2]);
+    expect(project).toEqual('');
 });
